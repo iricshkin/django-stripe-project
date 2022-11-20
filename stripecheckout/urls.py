0d1e670a7e7e38afb5d99fe23c76ccad8cab19c8
iricshkin <fokira@mail.ru>
2022-11-20 15:01:27 +0300

    create worlflows

--- stripecheckout/urls.py
+++ stripecheckout/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from stripecheckout import views
 
-app_name = 'stripecheckout'
+app_name = "stripecheckout"
 
 urlpatterns = [
     path("buy/<int:item_id>/", views.ItemBuyView.as_view()),
